9a64cb19d6018fc1170efa34a4793ba3057c4641
Tatarplusplus <romanbaygelden26@gmail.com>
2025-03-08 23:59:45 +0300

    start adding state

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,49 @@
 use axum::{response::Html, routing::any, Router};
-use tokio::{net::TcpListener, fs};   
-use std::io::Error; 
+use tokio::{net::TcpListener, sync::broadcast};
+use std::{include_str, sync::Arc};   
 // post locally, get only public
 
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    let root = root().await?;
-    let arsen = arsen().await?;
-    let svetov = svetov().await?;
-    let fallback = fallback().await?;
+struct AppState {
+    tx: broadcast::Sender<String>,
+}
 
+#[tokio::main]
+async fn main() {
     let authors_routes = Router::new()
-        .route("/", any(root))
+        .route("/", any(authors))
         .route("/arsen_markaryan", any(arsen))
         .route("/mikhail_svetov", any(svetov));
 
+    let (tx, _) = broadcast::channel(100);
+
+    let app_state = Arc::new(AppState { tx });
 
     let app = Router::new()
+        .route("/", any(root))
         .nest("/authors", authors_routes)
-        .fallback(fallback);
+        .fallback(fallback)
+        .with_state(app_state);
 
-    let listener = TcpListener::bind("127.0.0.1:7979").await?;
-
-    axum::serve(listener, app).await?;
-
-    Ok(())
+    let listener = TcpListener::bind("127.0.0.1:7979").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
 
-async fn root() -> Result<Html<String>, Error> {
-    let path = "htmls/authors.html";
-    let file = fs::read_to_string(path).await?;
-    
-    Ok(Html(file))
+async fn root() -> Html<&'static str> {
+    Html(include_str!("../htmls/root.html"))
+}
+
+async fn authors() -> Html<&'static str> {
+    Html(include_str!("../htmls/authors.html"))
 } 
 
-async fn arsen() -> Result<Html<String>, Error> {
-    let path = "htmls/arsen.html";
-    let file= fs::read_to_string(path).await?; 
-
-    Ok(Html(file))
+async fn arsen() -> Html<&'static str> {
+    Html(include_str!("../htmls/arsen.html"))
 }
 
-async fn svetov() -> Result<Html<String>, Error> {
-    let path = "htmls/svetov.html";
-    let file= fs::read_to_string(path).await?;
-
-    Ok(Html(file))
+async fn svetov() -> Html<&'static str> {
+    Html(include_str!("../htmls/svetov.html"))
 }
 
-async fn fallback() -> Result<Html<String>, Error> {
-    let path = "htmls/fallback.html";
-    let file= fs::read_to_string(path).await?;
-
-    Ok(Html(file))
+async fn fallback() -> Html<&'static str> {
+    Html(include_str!("../htmls/fallback.html"))
 }
