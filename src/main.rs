759e6a3d96e6f447cb831ffe001cd84ef6068a06
Tatarplusplus <romanbaygelden26@gmail.com>
2025-03-08 23:04:32 +0300

    rebuild structure

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,56 @@
-use axum::{Router, routing::any, response::Html};
-use tokio::{net::TcpListener, fs};    
+use axum::{response::Html, routing::any, Router};
+use tokio::{net::TcpListener, fs};   
+use std::io::Error; 
 // post locally, get only public
 
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/", any(root)).route("/arsen_markaryan", any(arsen)).route("/mikhail_svetov", any(svetov));
+async fn main() -> Result<(), Error> {
+    let root = root().await?;
+    let arsen = arsen().await?;
+    let svetov = svetov().await?;
+    let fallback = fallback().await?;
 
-    let listener = TcpListener::bind("127.0.0.1:7979").await.unwrap();
+    let authors_routes = Router::new()
+        .route("/", any(root))
+        .route("/arsen_markaryan", any(arsen))
+        .route("/mikhail_svetov", any(svetov));
 
-    axum::serve(listener,app).await.unwrap();
+
+    let app = Router::new()
+        .nest("/authors", authors_routes)
+        .fallback(fallback);
+
+    let listener = TcpListener::bind("127.0.0.1:7979").await?;
+
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
 
-async fn root() -> Html<&'static str> {
-    let path = "htmls/main.html";
-    let file: &'static str = fs::read_to_string(path).await.unwrap().leak();
-    Html(file)
+async fn root() -> Result<Html<String>, Error> {
+    let path = "htmls/authors.html";
+    let file = fs::read_to_string(path).await?;
+    
+    Ok(Html(file))
 } 
 
-async fn arsen() -> Html<&'static str> {
+async fn arsen() -> Result<Html<String>, Error> {
     let path = "htmls/arsen.html";
-    let file: &'static str = fs::read_to_string(path).await.unwrap().leak(); 
-    Html(file)
+    let file= fs::read_to_string(path).await?; 
+
+    Ok(Html(file))
 }
 
-async fn svetov() -> Html<&'static str> {
+async fn svetov() -> Result<Html<String>, Error> {
     let path = "htmls/svetov.html";
-    let file: &'static str = fs::read_to_string(path).await.unwrap().leak();
-    Html(file)
+    let file= fs::read_to_string(path).await?;
+
+    Ok(Html(file))
+}
+
+async fn fallback() -> Result<Html<String>, Error> {
+    let path = "htmls/fallback.html";
+    let file= fs::read_to_string(path).await?;
+
+    Ok(Html(file))
 }
